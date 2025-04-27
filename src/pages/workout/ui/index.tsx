03d545fac1bc9859dbc8d0710a6b246944c8eb17
Artem Sdobnikov <artem.sdobnikov25@gmail.com>
2025-04-27 13:06:58 +0300

    log on prod

--- src/pages/workout/ui/index.tsx
+++ src/pages/workout/ui/index.tsx
@@ -2,5 +2,5 @@ import { type PageProps } from '@/pages/types';
 import { type FC } from 'react';
 
 export const WorkoutPage: FC<PageProps> = () => {
-  return null;
+  return <pre>{JSON.stringify(window.location.href)}</pre>;
 };
