c5f0fadfed77dcf89aded0fe4c11b8211701ffcd
Artem Sdobnikov <artem.sdobnikov25@gmail.com>
2025-04-27 16:47:19 +0300

    try fix getting initData

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
-import { retrieveLaunchParams } from '@telegram-apps/sdk-react';
+// import { retrieveLaunchParams } from '@telegram-apps/sdk-react';
 
 import { EnvUnsupported } from '@/app/env-unsupported.tsx';
 import { init } from '@/init.ts';
@@ -10,21 +10,22 @@ import { ErrorBoundary } from './error-boundary.tsx';
 import { App } from './app.tsx';
 
 // mock the environment in case, we are outside Telegram.
-import '../mockEnv.ts';
+// import '../mockEnv.ts';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 try {
-  const launchParams = retrieveLaunchParams();
-  const { tgWebAppPlatform: platform } = launchParams;
-  const debug = (launchParams.tgWebAppStartParam || '').includes('debug') || import.meta.env.DEV;
+  // const launchParams = retrieveLaunchParams();
+  // const { tgWebAppPlatform: platform } = launchParams;
+  // const debug = (launchParams.tgWebAppStartParam || '').includes('debug') || import.meta.env.DEV;
 
   // configure all application dependencies.
   await init({
-    debug,
+    debug: false,
     eruda: false,
     // eruda: debug && ['ios', 'android', 'tdesktop'].includes(platform),
-    mockForMacOS: platform === 'macos',
+    // mockForMacOS: platform === 'macos',
+    mockForMacOS: false,
   }).then(() => {
     root.render(
       <StrictMode>
--- src/init.ts
+++ src/init.ts
@@ -1,17 +1,17 @@
 import {
-  setDebug,
-  mountBackButton,
-  restoreInitData,
+  // setDebug,
+  // mountBackButton,
+  // restoreInitData,
   init as initSDK,
-  mountMiniApp,
-  bindThemeParamsCssVars,
-  mountViewport,
-  bindViewportCssVars,
-  mockTelegramEnv,
-  type ThemeParams,
-  themeParamsState,
-  retrieveLaunchParams,
-  emitEvent,
+  // mountMiniApp,
+  // bindThemeParamsCssVars,
+  // mountViewport,
+  // bindViewportCssVars,
+  // mockTelegramEnv,
+  // type ThemeParams,
+  // themeParamsState,
+  // retrieveLaunchParams,
+  // emitEvent,
 } from '@telegram-apps/sdk-react';
 
 /**
@@ -23,55 +23,55 @@ export async function init(options: {
   mockForMacOS: boolean;
 }): Promise<void> {
   // Set @telegram-apps/sdk-react debug mode and initialize it.
-  setDebug(options.debug);
+  // setDebug(options.debug);
   initSDK();
 
   // Add Eruda if needed.
-  options.eruda &&
-    void import('eruda').then(({ default: eruda }) => {
-      eruda.init();
-      eruda.show();
-      eruda.position({ x: 25, y: 25 });
-    });
+  // options.eruda &&
+  //   void import('eruda').then(({ default: eruda }) => {
+  //     eruda.init();
+  //     eruda.show();
+  //     eruda.position({ x: 25, y: 25 });
+  //   });
 
   // Telegram for macOS has a ton of bugs, including cases, when the client doesn't
   // even response to the "web_app_request_theme" method. It also generates an incorrect
   // event for the "web_app_request_safe_area" method.
-  if (options.mockForMacOS) {
-    let firstThemeSent = false;
-    mockTelegramEnv({
-      onEvent(event, next) {
-        if (event[0] === 'web_app_request_theme') {
-          let tp: ThemeParams = {};
-          if (firstThemeSent) {
-            tp = themeParamsState();
-          } else {
-            firstThemeSent = true;
-            tp ||= retrieveLaunchParams().tgWebAppThemeParams;
-          }
-          return emitEvent('theme_changed', { theme_params: tp });
-        }
+  // if (options.mockForMacOS) {
+  //   let firstThemeSent = false;
+  //   mockTelegramEnv({
+  //     onEvent(event, next) {
+  //       if (event[0] === 'web_app_request_theme') {
+  //         let tp: ThemeParams = {};
+  //         if (firstThemeSent) {
+  //           tp = themeParamsState();
+  //         } else {
+  //           firstThemeSent = true;
+  //           tp ||= retrieveLaunchParams().tgWebAppThemeParams;
+  //         }
+  //         return emitEvent('theme_changed', { theme_params: tp });
+  //       }
 
-        if (event[0] === 'web_app_request_safe_area') {
-          return emitEvent('safe_area_changed', { left: 0, top: 0, right: 0, bottom: 0 });
-        }
+  //       if (event[0] === 'web_app_request_safe_area') {
+  //         return emitEvent('safe_area_changed', { left: 0, top: 0, right: 0, bottom: 0 });
+  //       }
 
-        next();
-      },
-    });
-  }
+  //       next();
+  //     },
+  //   });
+  // }
 
   // Mount all components used in the project.
-  mountBackButton.ifAvailable();
-  restoreInitData();
-  await Promise.all([
-    mountMiniApp.isAvailable() &&
-      mountMiniApp().then(() => {
-        bindThemeParamsCssVars();
-      }),
-    mountViewport.isAvailable() &&
-      mountViewport().then(() => {
-        bindViewportCssVars();
-      }),
-  ]);
+  // mountBackButton.ifAvailable();
+  // restoreInitData();
+  // await Promise.all([
+  //   mountMiniApp.isAvailable() &&
+  //     mountMiniApp().then(() => {
+  //       bindThemeParamsCssVars();
+  //     }),
+  //   mountViewport.isAvailable() &&
+  //     mountViewport().then(() => {
+  //       bindViewportCssVars();
+  //     }),
+  // ]);
 }
