4103bf7f9a0c929c0cc0523a88b6179a1da987c7
Artem Sdobnikov <artem.sdobnikov25@gmail.com>
2025-04-27 12:31:22 +0300

    remove logs

--- src/entities/user/model/auth.ts
+++ src/entities/user/model/auth.ts
@@ -1,13 +1,10 @@
-import { retrieveRawInitData, retrieveLaunchParams } from '@telegram-apps/sdk-react';
+import { retrieveRawInitData } from '@telegram-apps/sdk-react';
 import { api } from '@/shared/lib';
 import { createEffect, sample } from 'effector';
 import { getMeFx } from './user';
 
 const signInViaTelegramFx = createEffect(() => {
   const initData = retrieveRawInitData();
-  const lp = retrieveLaunchParams();
-
-  console.log({ initData, lp });
 
   if (!initData) {
     throw new Error('initData not available');
